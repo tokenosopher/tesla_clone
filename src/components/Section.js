fec3b90269e1806b63d4314a8776122f2ad48b95
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-05-02 14:44:56 +0100

    changed the path of images to fix gh-pages

--- src/components/Section.js
+++ src/components/Section.js
@@ -27,7 +27,7 @@ const Section = ({title, description, leftBtnText, rightBtnText, backgroundImg})
                         }
                     </ButtonGroup>
                 </Fade>
-                <DownArrow src={"/images/down-arrow.svg"}/>
+                <DownArrow src={"./images/down-arrow.svg"}/>
             </Buttons>
         </Wrap>
     )
